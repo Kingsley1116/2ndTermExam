d3628c90dbb4f598efa7a5d54ff8b773b6bb0efc
Kingsley1116 <93391504+Kingsley1116@users.noreply.github.com>
2023-03-14 20:26:55 +0800

    Update 5.py

--- s1b/雙數題/5.py
+++ s1b/雙數題/5.py
@@ -1,63 +1,56 @@
 # 登入帳號且增加改密碼選項（第四題進階版）
 
+# 設置默認用戶名和密碼
 defaultUserName = "admin"
 defaultPassword = "1234"
+# 設置登入狀態的變量，預設為False，表示未登入
+loggedIn = False
 
-def main():
+# 進入主循環
+while True:
     print("----------")
     print("1. 登入")
-    print("2. 退出系統")
-    choice = int(input())
+    print("2. 修改密碼")
+    print("3. 退出系統")
+    choice = int(input())  # 輸入用戶選擇的操作
 
-    if choice == 1:
-        system()
-    else:
-        exit("bye 9 bye!")
+    if choice == 1:  # 如果用戶選擇登入
+        i = 0  # 設置計數器，用於計算輸入用戶名和密碼的次數
+        while i < 3:  # 設置while循環
+            if i < 3:  # 如果輸入錯誤的次數小於3
+                inputUserName = input("請輸入用戶名: ")
+                inputPassword = input("請輸入密碼: ")
 
-def system():
-    global defaultPassword
-    for i in range(3):
-
-        inputUserName = input("請輸入用戶名: ")
-        inputPassword = input("請輸入密碼: ")
-
-        if inputUserName == defaultUserName:
-            if inputPassword == defaultPassword:
-                print("登入成功!")
-
-                while True:
-                    print("----------")
-                    print("1. 登出")
-                    print("2. 修改密碼")
-
-                    choice = int(input())
-
-                    if choice == 1:
-                        print("bye 9 bye!")
-                        main()
-
-                    elif choice == 2:
-                       changePassword() 
+                if inputUserName == defaultUserName:  # 如果輸入的用戶名正確
+                    if inputPassword == defaultPassword:  # 如果輸入的密碼正確
+                        print("登入成功!")
+                        loggedIn = True  # 登入狀態設為True
+                        break  # 跳出while循環
+                    else:
+                        print(f"密碼錯誤，剩下 {2 - i} 次機會!")
+                        i = i + 1  # 輸入錯誤，計數器+1
+                else:
+                    print(f"用戶名錯誤，剩下 {2 - i} 次機會!")
+                    i = i + 1  # 輸入錯誤，計數器+1
             else:
-                print(f"密碼錯誤，剩下 {2 - i} 次機會!")
+                print("3次機會已用盡!")  # 如果輸入錯誤的次數大於等於3，提示用戶已用盡機會
+
+    elif choice == 2:  # 如果用戶選擇修改密碼
+        if loggedIn:  # 如果已經登入
+            oldPassword = input("請輸入舊密碼: ")
+            newPassword = input("請輸入新密碼: ")
+            confirmPassword = input("請再次輸入新密碼: ")
+
+            if oldPassword == defaultPassword:  # 如果輸入的舊密碼正確
+                if newPassword == confirmPassword:  # 如果輸入的新密碼和確認新密碼一致
+                    defaultPassword = newPassword  # 將默認密碼設置為新密碼
+                    print("密碼修改成功!")
+                else:
+                    print("確認新密碼錯誤!")
+            else:
+                print("舊密碼錯誤!") # 如果輸入的舊密碼不正確，顯示錯誤訊息
         else:
-            print(f"用戶名錯誤，剩下 {2 - i} 次機會!")
-    print("3次機會已用盡!")
+            print("請先登入!") # 如果未登入，提示用戶需要先登入
 
-def changePassword():
-    global defaultPassword
-    oldPassword = input("請輸入舊密碼: ")
-    newPassword = input("請輸入新密碼: ")
-    confirmPassword = input("請再次輸入新密碼: ")
-
-    if oldPassword == defaultPassword:
-        if newPassword == confirmPassword:
-            defaultPassword = newPassword
-            print("密碼修改成功!")
-        else:
-            print("確認新密碼錯誤!")
-    else:
-        print("舊密碼錯誤!")
-
-if __name__ == "__main__":
-    main()
+    elif choice == 3: # 選擇退出系統
+        exit("再見!") # 退出程式，顯示再見訊息
